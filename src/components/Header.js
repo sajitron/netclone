3b1326531eb302c76924080c3851c50c824e1932
sajicode <sajioloye@gmail.com>
2018-12-25 17:04:01 +0100

    set up base styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-	<header>
-		<nav>
-			<div>
+	<header className="header">
+		<nav className="container">
+			<div className="header__content">
 				<NavLink to="/" exact={true}>
-					<h1 className="text-primary links">NetClone</h1>
+					<h1 className="header__title nav-left link">NetClone</h1>
 				</NavLink>
 
 				<div>
-					<NavLink to="/" activeClassName="is-active" exact={true}>
+					<NavLink to="/" activeClassName="is-active" className="link" exact={true}>
 						Home Page
 					</NavLink>
-					<NavLink to="/popular" activeClassName="is-active">
+					<NavLink to="/popular" activeClassName="is-active" className="link">
 						Popular Movies
 					</NavLink>
-					<NavLink to="/playing" activeClassName="is-active">
+					<NavLink to="/playing" activeClassName="is-active" className="link">
 						Now Playing Movies
 					</NavLink>
-					<NavLink to="/top" activeClassName="is-active">
+					<NavLink to="/top" activeClassName="is-active" className="link">
 						Top Rated Movies
 					</NavLink>
 				</div>
